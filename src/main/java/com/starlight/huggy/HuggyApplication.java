bad33a8ad047538fbc5621053026a8e452c5c5e7
Eundms <dms011221@gmail.com>
2023-02-01 22:01:55 +0900

    initial commit

---
+++ src/main/java/com/starlight/huggy/HuggyApplication.java
@@ -0,0 +1,13 @@
+package com.starlight.huggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuggyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlight/huggy/HuggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlight.huggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
